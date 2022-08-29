b5b98002129ecf53d5614cf03d163cd6199caf3c
ribicazlatan <70208896+ribicazlatan@users.noreply.github.com>
2022-08-29 21:06:56 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/chatapp/Profile.java
@@ -0,0 +1,32 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Profile extends AppCompatActivity {
+
+    private Button btnLogOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        btnLogOut = findViewById(R.id.btnLogOut);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Profile.this,MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                finish();
+            }
+        });
+    }
+}
